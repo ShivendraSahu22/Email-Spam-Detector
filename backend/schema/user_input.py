24c5cedaa0b0c8663cf141f6da2f069131f29047
ShivendraSahu22 <shivendrasahu518@gmail.com>
2026-07-11 00:27:19 +0530

    user input

---
+++ backend/schema/user_input.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel,Field, field_validator
+from typing import Annotated
+
+
+class UserInput(BaseModel):
+    message: Annotated[str, Field(description="The message to be analyzed for prediction")]
+
+    @field_validator("message")
+    def validate_message(cls, value):
+        if not value.strip():
+            raise ValueError("Message cannot be empty or whitespace.")
+        return value
